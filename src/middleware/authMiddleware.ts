04ae8ab355889cdaee53d541656536912f7664a0
Santiago Laterra <santigolaterra@gmail.com>
2025-12-03 14:22:07 -0300

    mejoras de validaciones en el authMiddleware, su formato y existencia del token

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -6,11 +6,21 @@ const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
   const SECRET_KEY = process.env.JWT_SECRET!
   const header = req.headers.authorization
 
+  //Si no exite la Secret_Key
+  if (!SECRET_KEY) {
+    return res.status(500).json({ success: false, error: "Error de configuración del servidor." })
+  }
+
   if (!header) {
     return res.status(401).json({ succes: false, error: "El token es requerido" })
   }
 
-  const token = header.split(" ")[1]
+  const [scheme, token] = header.split(" ")
+
+  //validarmos si el formato es "Bearer" y si hay un token
+  if (scheme !== "Bearer" || !token) {
+    return res.status(401).json({ success: false, error: "Formato de token inválido. Use Bearer <token>." })
+  }
 
   try {
     const payload = verify(token, SECRET_KEY);
