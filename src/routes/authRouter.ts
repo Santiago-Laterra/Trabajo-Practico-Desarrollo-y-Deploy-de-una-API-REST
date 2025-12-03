ecad794f57b00881bba380dfe5ab2205fa2def47
Santiago Laterra <santigolaterra@gmail.com>
2025-12-03 15:41:11 -0300

    Aplicaque el Rate Limit a ambas rutas de autenticación

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,11 +1,11 @@
 import { Router } from "express"
 import AuthController from "../controllers/authController"
+import limiter from "../middleware/rateLimitMiddleware"
 
 const authRouter = Router()
 
-// http://localhost:3000/auth/register
-authRouter.post("/register", AuthController.register)
-// http://localhost:3000/auth/login
-authRouter.post("/login", AuthController.login)
+
+authRouter.post("/register", limiter, AuthController.register)
+authRouter.post("/login", limiter, AuthController.login)
 
 export default authRouter
