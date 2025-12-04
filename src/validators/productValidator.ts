181d11b6664813c7322f0be3e6b892c7c679fa92
Santiago Laterra <santigolaterra@gmail.com>
2025-12-04 16:06:46 -0300

    actualizando el validator para mi nueva estructura

--- src/validators/productValidator.ts
+++ src/validators/productValidator.ts
@@ -1,11 +1,12 @@
 import { z } from "zod"
 
 const productSchemaValidator = z.object({
-  name: z.string().min(4),
-  description: z.string().min(10),
-  price: z.number().min(10, "El valor debe ser mayor a 10"),
-  category: z.string().min(2),
-  stock: z.number().positive()
+  title: z.string().min(3, "El titulo no puede estar vacio"),
+  synopsis: z.string(),
+  reting: z.number().min(0, "El rating mínimo es 0").max(10, "El rating máximo es 10"),
+  genre: z.string().min(2, "El género es demasiado corto"),
+  releaseYear: z.number().int("El año debe ser un número entero").min(1895, "El año debe ser posterior a 1895."),
+  director: z.string().min(3, "El nombre del director es muy corto")
 })
 
 export const createMovieSchema = productSchemaValidator
