18ce5b685cd4619143702a42a97db6d7267d9237
Santiago Laterra <santigolaterra@gmail.com>
2025-12-04 16:47:00 -0300

    arreglando un error de tipado en el validator

--- src/validators/movieValidator.ts
+++ src/validators/movieValidator.ts
@@ -3,7 +3,7 @@ import { z } from "zod"
 const productSchemaValidator = z.object({
   title: z.string().min(3, "El titulo no puede estar vacio"),
   synopsis: z.string(),
-  reting: z.number().min(0, "El rating mínimo es 0").max(10, "El rating máximo es 10"),
+  rating: z.number().min(0, "El rating mínimo es 0").max(10, "El rating máximo es 10"),
   genre: z.string().min(2, "El género es demasiado corto"),
   releaseYear: z.number().int("El año debe ser un número entero").min(1895, "El año debe ser posterior a 1895."),
   director: z.string().min(3, "El nombre del director es muy corto")
